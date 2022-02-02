beba588b2b7091f0e9427b1c14f72c390947c3d9
Victor Porton <porton.victor@gmail.com>
2022-02-02 19:37:41 +0200

    new API

--- src/lib.rs
+++ src/lib.rs
@@ -1,88 +1,124 @@
+use std::iter::{once, Once};
+
 /// A `Vec` inside together with positions that move together with the elements if the `Vec`
 /// has deletions or insertions.
 ///
 /// Implemented partially.
 
-pub struct VecWithPositions<T> {
-    vec: Vec<T>, // Apparently, I violated single-responsibility principle: any container-like object would suit.
-    positions: Vec<usize>,
-}
-
-impl<T> VecWithPositions<T> {
-    pub fn new() -> Self {
-        Self {
-            vec: Vec::new(),
-            positions: Vec::new(),
-        }
+pub trait VecWithPositions<'a, T, Positions: Iterator<Item = &'a mut usize>> {
+    fn vec(&self) -> &Vec<T>;
+    fn vec_mut(&mut self) -> &mut Vec<T>;
+    fn positions(&'a self) -> Box<dyn Iterator<Item = &'a usize> + 'a>;
+    fn positions_mut(&'a mut self) -> Box<dyn Iterator<Item = &'a mut usize> + 'a>;
+    fn push(&mut self, value: T) {
+        self.vec_mut().push(value)
     }
-    pub fn push(&mut self, value: T) {
-        self.vec.push(value)
+    fn append(&mut self, other: &mut Vec<T>) {
+        self.vec_mut().append(other)
     }
-    pub fn append(&mut self, other: &mut Vec<T>) {
-        self.vec.append(other)
-    }
-    pub fn remove(&mut self, index: usize) -> T {
-        let result = self.vec.remove(index);
-        for pos in self.positions.iter_mut() {
+    fn remove(&'a mut self, index: usize) -> T {
+        let result = self.vec_mut().remove(index);
+        for pos in self.positions_mut() {
             if *pos > index {
                 *pos -= 1;
             }
         }
         result
     }
-    pub fn clear(&mut self) {
-        self.vec.clear();
+    fn clear(&mut self) {
+        self.vec_mut().clear();
     }
-    pub fn get(&self, index: usize) -> Option<&T> {
-        self.vec.get(index)
+    fn get(&self, index: usize) -> Option<&T> {
+        self.vec().get(index)
     }
-    pub fn get_mut(&mut self, index: usize) -> Option<&mut T> {
-        self.vec.get_mut(index)
-    }
-
-    pub fn new_position(&mut self, value: usize) -> usize {
-        let pos = self.positions.len();
-        self.positions.push(value);
-        pos
-    }
-    pub fn move_position(&mut self, pos_index: usize, value: usize) {
-        self.positions[pos_index] = value;
-    }
-    pub fn position_index(&self, pos_index: usize) -> usize {
-        self.positions[pos_index]
+    fn get_mut(&mut self, index: usize) -> Option<&mut T> {
+        self.vec_mut().get_mut(index)
     }
 
-    pub fn is_empty(&self) -> bool {
-        self.vec.is_empty()
+    fn is_empty(&self) -> bool {
+        self.vec().is_empty()
     }
-    pub fn len(&self) -> usize {
-        self.vec.len()
+    fn len(&self) -> usize {
+        self.vec().len()
     }
 
-    pub fn iter(&self) -> impl Iterator<Item = &T> {
-        self.vec.iter()
+    fn iter(&'a self) -> std::slice::Iter<'a, T> {
+        self.vec().iter()
     }
-    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> {
-        self.vec.iter_mut()
+    fn iter_mut(&'a mut self) -> std::slice::IterMut<'a, T> {
+        self.vec_mut().iter_mut()
+    }
+}
+
+struct VecWithOnePosition<T> {
+    vec: Vec<T>,
+    position: usize,
+}
+
+impl<T> VecWithOnePosition<T> {
+    pub fn new() -> Self {
+        Self {
+            vec: Vec::new(),
+            position: 0
+        }
+    }
+    pub fn get_position(&self) -> usize {
+        self.position
+    }
+    pub fn set_position(&mut self, pos: usize) {
+        self.position = pos;
+    }
+}
+
+impl<'a, T> VecWithPositions<'a, T, Once<&'a mut usize>> for VecWithOnePosition<T> {
+    fn vec(&self) -> &Vec<T> {
+        &self.vec
+    }
+    fn vec_mut(&mut self) -> &mut Vec<T> {
+        &mut self.vec
+    }
+    fn positions(&'a self) -> Box<dyn Iterator<Item = &'a usize> + 'a> {
+        Box::new(once(&self.position))
+    }
+    fn positions_mut(&'a mut self) -> Box<dyn Iterator<Item = &'a mut usize> + 'a>  {
+        Box::new(once(&mut self.position))
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::VecWithPositions;
+    use crate::{VecWithOnePosition, VecWithPositions};
 
     #[test]
-    fn test() {
-        let mut v = VecWithPositions::new();
+    fn before() {
+        let mut v = VecWithOnePosition::new();
         let mut input = (0..10).collect::<Vec<i32>>();
         v.append(&mut input);
-        let mut _pos3 = v.new_position(3);
-        let mut _pos5 = v.new_position(5);
-        let mut _pos7 = v.new_position(7);
+        v.set_position(3);
         v.remove(5);
-        assert_eq!(v.iter().map(|n| *n).collect::<Vec<i32>>(), vec![0,1,2,3,4,6,7,8,9]);
-        assert_eq!(v.position_index(0), 3);
-        assert_eq!(v.position_index(1), 5);
-        assert_eq!(v.position_index(2), 6);
+        assert_eq!(v.iter().map(|n| *n).collect::<Vec<i32>>(), vec![0, 1, 2, 3, 4, 6, 7, 8, 9]);
+        assert_eq!(v.get_position(), 3);
+    }
+
+    #[test]
+    fn middle() {
+        let mut v = VecWithOnePosition::new();
+        let mut input = (0..10).collect::<Vec<i32>>();
+        v.append(&mut input);
+        v.set_position(5);
+        v.remove(5);
+        assert_eq!(v.iter().map(|n| *n).collect::<Vec<i32>>(), vec![0, 1, 2, 3, 4, 6, 7, 8, 9]);
+        assert_eq!(v.get_position(), 5);
+    }
+
+    #[test]
+    fn after() {
+        let mut v = VecWithOnePosition::new();
+        let mut input = (0..10).collect::<Vec<i32>>();
+        v.append(&mut input);
+        v.set_position(7);
+        v.remove(5);
+        assert_eq!(v.iter().map(|n| *n).collect::<Vec<i32>>(), vec![0, 1, 2, 3, 4, 6, 7, 8, 9]);
+        assert_eq!(v.get_position(), 6);
     }
 }
