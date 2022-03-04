dc0d7b957db59743b32d1c58f1b6aa46f02a79ef
Victor Porton <porton.victor@gmail.com>
2022-03-04 20:10:24 +0200

    src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -14,77 +14,14 @@ pub trait ActiveResource: Clone {
     fn pos_index(&self) -> usize;
 }
 
-pub struct VecWithPositionsVector<Active: ActiveResource, Inactive> {
+pub struct VecWithPosition<Active: ActiveResource, Inactive> {
     inactive: Vec<Inactive>,
-    active: Vec<Active>,
+    active: Option<Position>,
 }
 
-impl<Active: ActiveResource, Inactive> Default for VecWithPositionsVector<Active, Inactive> {
+impl<Active: ActiveResource, Inactive> Default for VecWithPosition<Active, Inactive> {
     fn default() -> Self {
         Self::new()
-    }
-}
-
-impl<Active: ActiveResource, Inactive> VecWithPositionsVector<Active, Inactive> {
-    pub fn new() -> Self {
-        Self {
-            inactive: Vec::new(),
-            active: Vec::new(),
-        }
-    }
-
-    pub fn remove(&mut self, active: &Active) -> Inactive { // TODO: Duplicate code.
-        let pos = *self.active[active.pos_index()].position();
-        let result = self.inactive.remove(pos.0);
-        self.active.iter_mut().for_each(|p| {
-            let mut p2 = p.position_mut();
-            if p2.0 > pos.0 {
-                p2.0 -= 1;
-            }
-        });
-        self.active.remove(active.pos_index());
-        result
-    }
-    pub fn push(&mut self, value: Inactive) {
-        self.inactive.push(value)
-    }
-    pub fn append(&mut self, other: &mut Vec<Inactive>) {
-        self.inactive.append(other)
-    }
-
-    pub fn get_inactive(&self, pos: Position) -> Option<&Inactive> {
-        self.inactive.get(pos.0)
-    }
-    pub fn get_mut_inactive(&mut self, pos: Position) -> Option<&mut Inactive> {
-        self.inactive.get_mut(pos.0)
-    }
-    pub fn set_inactive(&mut self, pos: Position, value: Inactive) {
-        self.inactive[pos.0] = value;
-    }
-
-    pub fn get_active(&self, pos_index: usize) -> Option<Active> {
-        self.active.get(pos_index).map(|v| v.clone())
-    }
-    // pub fn set_active(&mut self, active: &Active, value: Active) {
-    //     self.active[active.pos_index()] = value;
-    // }
-
-    pub fn inactive_is_empty(&self) -> bool {
-        self.inactive.is_empty()
-    }
-    pub fn inactive_len(&self) -> usize {
-        self.inactive.len()
-    }
-
-    pub fn clear(&mut self) {
-        self.inactive.clear();
-        self.active.clear();
-    }
-    pub fn active_len(&self) -> usize {
-        self.active.len()
-    }
-    pub fn active_is_empty(&self) -> bool {
-        self.active.is_empty()
     }
 }
 
@@ -100,8 +37,7 @@ impl<Active: ActiveResource, Inactive> VecWithPositionsVector<Active, Inactive> 
 /// TODO: Test it.
 pub struct ResourcePool<Active: ActiveResource, Inactive: Clone> {
     inactive: Vec<Inactive>,
-    active: Vec<Active>,
-    next: Option<Position>, // wraps around circularly
+    active: Option<Active>,
     allocator: Box<dyn Fn(Inactive, Position, usize) -> Pin<Box<dyn Future<Output = Active> + Send + Sync>> + Send + Sync>,
 }
 
@@ -109,35 +45,25 @@ impl<'a, Active: ActiveResource, Inactive: Clone> ResourcePool<Active, Inactive>
     pub fn new(allocator: Box<dyn Fn(Inactive, Position, usize) -> Pin<Box<dyn Future<Output = Active> + Send + Sync>> + Send + Sync>) -> Self {
         Self {
             inactive: Vec::new(),
-            active: Vec::new(),
-            next: None,
+            active: None,
             allocator,
         }
     }
 
     pub fn push(&mut self, value: Inactive) {
         self.inactive.push(value);
-        if self.next.is_none() {
-            self.next = Some(Position(0));
-        }
     }
     pub fn append(&mut self, other: &mut Vec<Inactive>) {
         self.inactive.append(other);
-        if self.next.is_none() {
-            self.next = Some(Position(0));
-        }
     }
-    pub async fn remove(&mut self, active: &Active) -> Option<Active> { // TODO: Duplicate code.
-        let pos = *self.active[active.pos_index()].position();
-        self.inactive.remove(pos.0);
-        self.active.iter_mut().for_each(|p| {
-            let mut p2 = p.position_mut();
-            if p2.0 > pos.0 {
-                p2.0 -= 1;
-            }
-        });
-        self.reallocate_position(active).await
-
+    pub async fn remove(&mut self) -> Option<Active> { // TODO: Duplicate code.
+        if let Some(ref active) = self.active {
+            let result = self.next(); // self.reallocate_position(active).await;
+            self.inactive.remove(active.position().0);
+            result
+        } else {
+            None
+        }
     }
     pub fn inactive_len(&self) -> usize {
         self.inactive.len()
@@ -158,11 +84,7 @@ impl<'a, Active: ActiveResource, Inactive: Clone> ResourcePool<Active, Inactive>
 
     /// Allocates a resource if there are free resources.
     pub async fn allocate_new_position(&mut self) -> Option<usize> {
-        if self.active.len() >= self.inactive.len() {
-            None
-        } else {
-            self.allocate_rapacious().await
-        }
+        self.allocate_rapacious().await
     }
     /// Allocates a resource even if all resources are busy.
     pub async fn allocate_rapacious(&mut self) -> Option<usize> {
